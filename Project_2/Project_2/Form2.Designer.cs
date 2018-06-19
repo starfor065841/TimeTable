515af8e9c9022bb34a2fee5508cb6cccafd39f00
starfor065841 <player210421@gmail.com>
2018-06-19 21:58:14 +0800

    加上爬蟲
    多了日期 和 教室

--- Project_2/Project_2/Form2.Designer.cs
+++ Project_2/Project_2/Form2.Designer.cs
@@ -39,18 +39,18 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(210, 247);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Location = new System.Drawing.Point(280, 309);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(76, 22);
+            this.textBox1.Size = new System.Drawing.Size(100, 25);
             this.textBox1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(220, 55);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Location = new System.Drawing.Point(293, 69);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(56, 22);
+            this.button1.Size = new System.Drawing.Size(75, 28);
             this.button1.TabIndex = 1;
             this.button1.Text = "確定";
             this.button1.UseVisualStyleBackColor = true;
@@ -58,19 +58,18 @@
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(33, 114);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(44, 142);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(22, 24);
+            this.label1.Size = new System.Drawing.Size(29, 30);
             this.label1.TabIndex = 2;
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(220, 114);
-            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Location = new System.Drawing.Point(293, 142);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(56, 27);
+            this.button2.Size = new System.Drawing.Size(75, 34);
             this.button2.TabIndex = 3;
             this.button2.Text = "顏色";
             this.button2.UseVisualStyleBackColor = true;
@@ -79,9 +78,10 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(33, 27);
+            this.label2.Location = new System.Drawing.Point(44, 34);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(89, 12);
+            this.label2.Size = new System.Drawing.Size(112, 15);
             this.label2.TabIndex = 4;
             this.label2.Text = "請輸入課程名稱";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -89,33 +89,24 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "離散數學",
-            "計算機組織 ",
-            "演算法 ",
-            "視窗程式設計 ",
-            "自然語言技術及對話系統開發",
-            "JAVA軟體開發",
-            "類比電子電路實作 ",
-            "資訊工程倫理與生涯規劃 ",
-            "自由軟體開發與社群發展 "});
-            this.comboBox1.Location = new System.Drawing.Point(35, 55);
+            this.comboBox1.Location = new System.Drawing.Point(47, 69);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.Size = new System.Drawing.Size(190, 23);
             this.comboBox1.TabIndex = 5;
             // 
             // Form2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(335, 339);
+            this.ClientSize = new System.Drawing.Size(447, 424);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form2";
             this.Text = "Form2";
             this.Load += new System.EventHandler(this.Form2_Load);
--- Project_2/Project_2/Form2.cs
+++ Project_2/Project_2/Form2.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using HtmlAgilityPack;
+using System.Collections;
+
 
 namespace Project_2
 {
@@ -14,18 +18,26 @@ namespace Project_2
     {
         private Form1 f;
         String txt;
-        string[] classroom;
+        ArrayList date = new ArrayList();
+        ArrayList classroom = new ArrayList();
         public Form2(Form1 f1)
         {
             InitializeComponent();
             this.f = f1;
+
+           
+
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             txt = comboBox1.SelectedItem.ToString();
-            f.table[f.start_x, f.start_y].Text = txt;
-            f.table[f.start_x, f.start_y].Text += "\r\n"+classroom[comboBox1.SelectedIndex];
+            f.table[f.start_x, f.start_y].Text = ""  + date[comboBox1.SelectedIndex];
+            f.table[f.start_x, f.start_y].Text += "\r\n";
+            f.table[f.start_x, f.start_y].Text += "\r\n" + txt;
+            f.table[f.start_x, f.start_y].Text += "\r\n";
+            f.table[f.start_x, f.start_y].Text += "\r\n" + classroom[comboBox1.SelectedIndex];
             f.table[f.start_x, f.start_y].BackColor = label1.BackColor;
             //f.f2.Dispose();            
             this.Dispose();
@@ -49,9 +61,40 @@ namespace Project_2
 
         private void Form2_Load(object sender, EventArgs e)
         {
-            textBox1.Enabled = false;
+            HtmlWeb client = new HtmlWeb();
+            HtmlAgilityPack.HtmlDocument doc = client.Load("http://course-query.acad.ncku.edu.tw/qry/qry001.php?dept_no=F7");
+
+
+            for (int i = 1; i < 63; ++i)
+            {
+                if ((i % 11) != 0)
+                {
+                    string title = doc.DocumentNode.SelectSingleNode("//html / body / table / tbody / tr[" + i + "] / td[11]").InnerText;
+                    comboBox1.Items.Add(title);
+                }
+            }
             comboBox1.SelectedIndex = 0;
-            classroom = new string[9]{"65405","65405","65405","65203","65203","65304","65203","4263","65203" };
+
+            for (int i = 1; i < 63; ++i)
+            {
+                if ((i % 11) != 0)
+                {
+                    string title = doc.DocumentNode.SelectSingleNode("//html/body/table/tbody/tr[" + i +"]/td[17]").InnerText;
+                    date.Add(title);
+                }
+            }
+            for (int i = 1; i < 63; ++i)
+            {
+                if ((i % 11) != 0)
+                {
+                    string title = doc.DocumentNode.SelectSingleNode("//html/body/table/tbody/tr[" + i + "]/td[18]").InnerText;
+                    classroom.Add(title);
+                }
+            }
+            //comboBox1.SelectedIndex = comboBox1.Items.Count - 1;
+            //textBox1.Enabled = false;
+            //comboBox1.SelectedIndex = 0;
+            //classroom = new string[9]{"65405","65405","65405","65203","65203","65304","65203","4263","65203" };
         }
     }
 }
